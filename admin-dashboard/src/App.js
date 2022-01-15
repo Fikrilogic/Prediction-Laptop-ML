65a13ec2b2dd7b8a3129eda33267076b5d17dc88
Dandi Sabilli Izzulhaq <45232854+dandiabil@users.noreply.github.com>
2022-01-15 13:45:19 +0700

    Adding Routes

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,51 +1,77 @@
 import theme from "./theme";
-import {ThemeProvider} from "@mui/styles";
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
-import {useReducer} from "react";
+import { ThemeProvider } from "@mui/styles";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { RouteObject } from "react-router-dom";
+import { useReducer } from "react";
 
-import {AuthContext} from "./Context/context";
-import {Reducer, AUTH_STATE} from "./Context/reducer";
+import { AuthContext } from "./Context/context";
+import { Reducer, AUTH_STATE } from "./Context/reducer";
 
-import LoginDashboard from "./pages/LoginDashboard/login-dashboard";
-import UserDashboard from "./pages/Dashboard/user-dashboard.jsx"
-import DatasetDashboard from './pages/Dashboard/dataset-dashboard.jsx'
+import LoginDashboard from "./pages/LoginDashboard/Login-Dashboard";
+import UserDashboard from "./pages/Dashboard/User-Dashboard.jsx";
+import DatasetDashboard from "./pages/Dashboard/Dataset-Dashboard.jsx";
 import PrivateRoute from "./components/ProtectedRoute/protect-route.component";
+import Layout from "./pages/Layout";
+import AccountDetail from "./pages/AccountDetail/Account-Detail";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import DatasetTablesDashboard from "./pages/DatasetTables/Dataset-Tables-Dashboard";
 
 function App(props) {
-    const [user, dispatch] = useReducer(Reducer, AUTH_STATE);
-    console.log("app:", props);
+  const [user, dispatch] = useReducer(Reducer, AUTH_STATE);
+  console.log("app:", props);
 
-    return (
-        <ThemeProvider theme={theme}>
-            <AuthContext.Provider value={{user, dispatch}}>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Navigate to="/login"/>}/>
-                        <Route path="login" element={<LoginDashboard/>}/>
-
-                        <Route path="dashboard">
-                            <Route
-                                path="user/*"
-                                element={
-                                    <PrivateRoute>
-                                        <UserDashboard/>
-                                    </PrivateRoute>
-                                }
-                            />
-                            <Route
-                                path='dataset/*'
-                                element={
-                                    <PrivateRoute>
-                                        <DatasetDashboard/>
-                                    </PrivateRoute>
-                                }
-                            />
-                        </Route>
-                    </Routes>
-                </BrowserRouter>
-            </AuthContext.Provider>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <AuthContext.Provider value={{ user, dispatch }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="login" element={<LoginDashboard />} />
+            <Route
+              path="dashboard"
+              element={
+                <PrivateRoute>
+                  <Layout element={<Dashboard />} />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="dataset/tables"
+              element={
+                <PrivateRoute>
+                  <Layout element={<DatasetTablesDashboard />} />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="user"
+              element={
+                <PrivateRoute>
+                  <Layout element={<UserDashboard />} />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="dataset"
+              element={
+                <PrivateRoute>
+                  <Layout element={<DatasetDashboard />} />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="profile"
+              element={
+                <PrivateRoute>
+                  <Layout element={<AccountDetail />} />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </AuthContext.Provider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
